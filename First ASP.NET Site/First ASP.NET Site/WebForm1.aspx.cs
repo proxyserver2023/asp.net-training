f489efd55d5caac7b2f2e2042e6101b2dabc2475
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-09-18 21:48:40 +0600

    Add(project-files) - First Project with ASP.NET using Visual C#

---
+++ First ASP.NET Site/First ASP.NET Site/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First_ASP.NET_Site
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected string var1 = "Hello From C#";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
