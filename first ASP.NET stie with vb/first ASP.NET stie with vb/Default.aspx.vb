0456c852cbd002a2f75c7fa99414d9578e19d903
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-11-08 09:21:57 +0600

    Recording all the removals

--- first ASP.NET stie with vb/first ASP.NET stie with vb/Default.aspx.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class _Default
-    Inherits Page
-    Protected _var1 As String = "Hello World"
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
-    End Sub
-End Class
--- first ASP.NET stie with vb/first ASP.NET stie with vb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="first_ASP.NET_stie_with_vb.Global_asax" Language="vb" %>
